55c27c1d4b12bb683180378e3cd9f7639f76056a
David Conner <agilebluefox@gmail.com>
2016-06-07 22:58:12 -0400

    Added functions to watch for click events on the contact list items and render some html dummy text for now.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
-// Initialize Variables
-var contacts = [],
-form;
+// Initialize global contact variables.
+var id, firstname, lastname, address1, city, state, phone, email;
+// Get the DOM elements
+var form = $('form');
 
-// Instill some dummy data to practice displaying stuff.
-contacts = [
+// Include some dummy data to practice displaying stuff.
+var contacts = [
         {
+           id: 0,
            firstname: 'Poindexter',
            lastname: 'Brooks',
            address1: '101 Smart Lane',
@@ -15,6 +17,7 @@ contacts = [
         },
 
         {
+            id: 1,
             firstname: 'John',
             lastname: 'Smith',
             address1: '3570 Wonder Where Drive',
@@ -25,6 +28,7 @@ contacts = [
         },
 
         {
+            id: 2,
             firstname: 'Ralph',
             lastname: 'Turner',
             address1: '3644 Open Road Ave.',
@@ -35,6 +39,7 @@ contacts = [
         },
 
         {
+            id: 3,
             firstname: 'Neal',
             lastname: 'Luck',
             address1: '666 Underworld Lane',
@@ -48,25 +53,68 @@ contacts = [
 $(document).ready(pageLoad);
 // Run this when the page loads.
 function pageLoad() {
-    // Get the form element
-    form = $('form');
+    buildList();
+
     // Event handlers
     form.submit(function(event){
         event.preventDefault();
+        // Put the form fields in a JSON object.
         var fields = $(this).serializeArray();
         addContact(fields);
+        this.reset();
+    });
+    $('ul.contact-list').on('click', 'li a', function(event){
+        event.preventDefault();
+        id = $(this).attr('href');
+        console.log("The id value is: " + id);
+        showContact(id);
     });
 }
+
+// Function to load the selected contact's information.
+function showContact(id) {
+    var selectedContact = $.grep(contacts, function(contact) {return(contact.id === 3);
+    var info = $('ul.contact-info');
+    info.html('<li class="first-name">First Name: Some</li><li class="last-name">Last Name: Friend</li><li class="phone-number">Phone Number: 555-555-5555</li><li>Address:</li><ul><li class="address">123 No Way Lane, Wild Town, State</li></ul>');
+}
+
 // Get the data from the form, create a new contact object and store it
 // in the contacts array.
 function addContact(fields) {
+    // Constructor for the Person object.
+    var Person = function() {
+        this.id = contacts.length;
+    };
+    // Make a new contact object.
     var contact = new Person();
-    // Get the values submitted.
+    // Get the values submitted and insert them into the object.
     $.each(fields, function(i, fields) {
         contact[fields.name] = fields.value;
     });
     // Add the new contact object to the array of contacts.
     contacts.push(contact);
+    addToList(contact);
 }
 
-var Person = function() {};
+// Render the names to the list of contacts.
+function buildList() {
+    for (var i = 0; i < contacts.length; i++)
+    {
+        id = contacts[i].id;
+        firstname = contacts[i].firstname;
+        lastname = contacts[i].lastname;
+        renderListItem(id, firstname, lastname);
+    }
+}
+
+// Add the newly added name to the contacts list.
+function addToList(contact) {
+    id = contact.id;
+    firstname = contact.firstname;
+    lastname = contact.lastname;
+    renderListItem(id, firstname, lastname);
+}
+
+function renderListItem(id, first, last) {
+    $('ul.contact-list').append('<li><a href="' + id + '">' + first + " " + last + '</a></li>');
+}
